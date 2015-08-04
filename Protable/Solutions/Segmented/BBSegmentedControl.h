5d18e794811fc4a0ee91b6514ed53955b39deb33
houzhenyong <houzhenyong_dev@126.com>
2015-08-04 11:46:10 +0800

    增加注释

--- Protable/Solutions/Segmented/BBSegmentedControl.h
+++ Protable/Solutions/Segmented/BBSegmentedControl.h
@@ -10,7 +10,8 @@
 
 /**
  *  BBSegmentedControlScrollStyle
- *  两种不同的样式：一种灵感来自于QQ音乐，一种灵感来自于美拍
+ *  规定了两种不同的样式，一种灵感来自于QQ音乐（BBSegmentedControlScrollStyle未定义），一种灵感来自于美拍（BBSegmentedControlScrollStyle定义）.
+ *  适用于不同场景，当segmented item id个数使得屏幕宽度能装下时，使用美拍式，否则，使用QQ音乐式更好。
  */
 //#define BBSegmentedControlScrollStyle  1
 
